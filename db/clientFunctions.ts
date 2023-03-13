bc0067c6ee0da9a9e5e0114c5715b8ca769cdc16
Peyton Cornelison <pcornelison@inceptionllc.com>
2023-03-13 16:20:05 -0500

    Another test

--- db/clientFunctions.ts
+++ db/clientFunctions.ts
@@ -31,5 +31,6 @@ export const findClient = async (userId, name, email) => {
 
 export const insertClientDocument = async (email: string, documentId: any) => {
   const client = await clientModel.findOneAndUpdate({email: email}, {$push: {documents: documentId}})
+  console.log('test')
   return client;
 }
